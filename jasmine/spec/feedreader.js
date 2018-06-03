3f64aadbccf9cf2d4d5e5b9551d8d3a8653d4138
Scott Henning <scott@scotthenning.info>
2018-06-03 15:07:47 -0500

    completed feedreader testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -62,6 +62,10 @@ $(function() {
        * hiding/showing of the menu element.
        */
         it('is hidden by default', function() {
+          /* because feedreader uses a CSS class placed on the body
+           * to move the menu offscreen... we test to see if said class
+           * is placed on the body element by default.
+           */
           expect(docBody.classList).toContain('menu-hidden');
         });
 
@@ -72,8 +76,14 @@ $(function() {
        */
       it('changes visibility on menu button click', function() {
         const menuIcon = $('.menu-icon-link');
+        /* programatically 'click' the menu icon to see if the 'menu hiding'
+         * class is removed, thus displaying the menu
+         */
         menuIcon.trigger("click");
         expect(docBody.classList).not.toContain('menu-hidden');
+        /* programatically 'click' the menu icon again to see if the 'menu hiding'
+         * class is added again to the body, thus hiding the menu
+         */
         menuIcon.trigger("click");
         expect(docBody.classList).toContain('menu-hidden');
       });
@@ -90,16 +100,20 @@ $(function() {
       * Remember, loadFeed() is asynchronous so this test will require
       * the use of Jasmine's beforeEach and asynchronous done() function.
       */
+      var feed = document.querySelector('.feed'),
+          feedItems;
       beforeEach(function(done) {
+        /* run the loadFeed function and use the callback to grab the feed
+         * entries after they have loaded
+         */
         loadFeed(0, function() {
+          feedItems = feed.querySelectorAll('.entry');
           done();
         });
       });
       it('are visible', function(done) {
-       const feedDiv = document.querySelector('.feed');
-       const feedHTML = feedDiv.innerHTML;
-       console.log(feedHTML);
-       //expect();
+        // Make sure there is at least one feed entry that has loaded into the feeder
+        expect(feedItems.length > 0).toBeTruthy();
        done();
       });
     });
@@ -110,8 +124,33 @@ $(function() {
       * by the loadFeed function that the content actually changes.
       * Remember, loadFeed() is asynchronous.
       */
-     it('loads successfully', function() {
+      var feed = document.querySelector('.feed'),
+          initialFeedContent, newFeedContent,
+          initialFirstFeedTitle, newFirstFeedTitle;
 
+      beforeEach(function(done) {
+        // load initial feed
+        loadFeed(0, function() {
+          // grab all initial feed articles with the class of 'entry'
+          initialFeedContent = feed.querySelectorAll('.entry');
+          // grab the h2 of the initial feed's first article.entry element
+          initialFirstFeedTitle = initialFeedContent[0].querySelector('h2').textContent;
+        });
+        // load a new feed
+        loadFeed(1, function() {
+          // grab all new feed articles with the class of 'entry'
+          newFeedContent = feed.querySelectorAll('.entry');
+          // grab the h2 of the new feed's first article.entry element
+          newFirstFeedTitle = newFeedContent[0].querySelector('h2').textContent;
+          done();
+        });
+      });
+
+
+
+     it('loads different content successfully', function() {
+
+       expect(initialFirstFeedTitle).not.toEqual(newFirstFeedTitle);
      });
     });
 }());
